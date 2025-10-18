f852eb76f304dc516e3075614873d9f1a97e49e8
abdulodhood003 <abdulodudood@gmail.com>
2025-10-18 15:12:52 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
      'http://localhost:5174',
-     'https://aurafit-frontend-git-main-abdul-odudoods-projects.vercel.app'
+     'https://aurafit-frontend.vercel.app'
 
     
   
